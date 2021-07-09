5660ab29209c3dfbd279ac815e16f4987b820345
rk <rk@desktoppc.com>
2021-07-09 12:57:37 +0545

    added temp_insuree_reg

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,5 +1,7 @@
+from django.urls import path
+from . import views
+
+
 urlpatterns = [
+    path('temp_insuree_reg/',views.temp_insuree_reg)
 ]
-
-
-
--- webapp/views.py
+++ webapp/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def temp_insuree_reg(request):
+    return render(request, 'temp_insuree_reg.html', {})
