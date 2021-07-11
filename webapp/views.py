398cffdaaf3d197e8f3394989f7ab99c5a25ab67
rk <rk@desktoppc.com>
2021-07-11 15:27:56 +0545

    xframe_options_exempt

--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.clickjacking import xframe_options_exempt
+
+@xframe_options_exempt
 def temp_insuree_reg(request):
+    response = render(request, 'temp_insuree_reg.html', {})
+    #response['X-Frame-Options'] = 'no-cache'
+    return response
+    #return render(request, 'temp_insuree_reg.html', {})
+
+def temp_insuree_reg_html(request):
     return render(request, 'temp_insuree_reg.html', {})
